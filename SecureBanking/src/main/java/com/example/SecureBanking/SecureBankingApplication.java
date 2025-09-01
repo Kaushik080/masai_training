1fb3afd1cd793b3b3cbed8057de2feef766eeba1
Kaushik080 <kaushikngage@gmail.com>
2025-09-01 21:28:10 +0530

    Secure banking api

---
+++ SecureBanking/src/main/java/com/example/SecureBanking/SecureBankingApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecureBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBankingApplication.class, args);
+	}
+
+}
---
+++ SecureBanking/src/test/java/com/example/SecureBanking/SecureBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
